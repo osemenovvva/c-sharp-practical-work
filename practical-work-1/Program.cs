4e367b72c5db92ff348342ed9372da3577c555a1
Olga Semenova <o.semenovvva@gmail.com>
2022-08-31 14:46:38 +0300

    add solution for practical work 1

---
+++ practical-work-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Hello World!!!");
+
+            Console.ReadKey();
+
+            //Задание 2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
