cdb179a77db54d9d2777d6351cfc9a7f98086f41
Olga Semenova <o.semenovvva@gmail.com>
2023-02-13 22:22:10 +0800

    add solution for practical work 9

---
+++ practical-work-9/WordsInStringWPF/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WordsInString;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickSplitWords(object sender, RoutedEventArgs e)
+        {
+            SplitWordsListBox.ItemsSource = WordsInString.Program.GetWords(SplitWordsTextBox.Text);
+        }
+
+        private void ButtonClickReverseWords(object sender, RoutedEventArgs e)
+        {
+            ReverseLabel.Content = Program.ReverseWords(ReverseTextBox.Text);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
